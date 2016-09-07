e62193cf59d7cddc83a07d0b6615d62fb494468d
Call3z <electrodez@gmail.com>
2016-09-07 13:58:21 +0200

    Button Message

--- WindowsFormsCalle/WindowsFormsCalle/Form1.cs
+++ WindowsFormsCalle/WindowsFormsCalle/Form1.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsCalle
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string summan = textBox1.Text + " " + textBox2.Text;
+            MessageBox.Show(summan,"Message");
+
+        }
     }
 }
