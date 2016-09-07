1b9ec94fc4dc2b376043331fe01065bc3fedf5f5
Call3z <electrodez@gmail.com>
2016-09-07 13:59:56 +0200

    Tagit bort. Börjar på ny uppgift

--- WindowsFormsCalle/WindowsFormsCalle/Form1.cs
+++ WindowsFormsCalle/WindowsFormsCalle/Form1.cs
@@ -16,12 +16,5 @@ namespace WindowsFormsCalle
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string summan = textBox1.Text + " " + textBox2.Text;
-            MessageBox.Show(summan,"Message");
-
-        }
     }
 }
