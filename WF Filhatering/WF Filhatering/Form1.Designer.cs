79d27fd9b96a232f7a201d1c661ede9e5c534c06
Call3z <electrodez@gmail.com>
2016-09-07 13:29:42 +0200

    Test

--- WF Filhatering/WF Filhatering/Form1.Designer.cs
+++ WF Filhatering/WF Filhatering/Form1.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox0 = new System.Windows.Forms.TextBox();
+            this.button0 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox0
+            // 
+            this.textBox0.Location = new System.Drawing.Point(125, 590);
+            this.textBox0.Name = "textBox0";
+            this.textBox0.Size = new System.Drawing.Size(599, 26);
+            this.textBox0.TabIndex = 0;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(125, 676);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(102, 40);
+            this.button0.TabIndex = 1;
+            this.button0.Text = "Visa";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1209, 821);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.textBox0);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox0;
+        private System.Windows.Forms.Button button0;
     }
 }
 
--- WF Filhatering/WF Filhatering/Form1.cs
+++ WF Filhatering/WF Filhatering/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,19 @@ namespace WF_Filhatering
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox0.Text = "Tjenare!";
+            SaveToFile();
+        }
+
+        public static void SaveToFile()
+        {
+            string[] lines = {"Tjenare", "vad heter", "du?"};
+
+            File.WriteAllLines(@"c:\test.txt", lines);
         }
     }
 }
