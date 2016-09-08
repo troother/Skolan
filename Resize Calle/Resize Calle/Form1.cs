1dd240b693055ff9172c9bd4a69a8b8c3109eab3
Call3z <electrodez@gmail.com>
2016-09-08 15:28:49 +0200

    Resize Calle

---
+++ Resize Calle/Resize Calle/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resize_Calle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Left = 80;
+            textBox1.Top = 80;
+        }
+
+
+        private void formen_resize(object sender, EventArgs e)
+        {
+            textBox1.Height = this.Height - 200;
+            textBox1.Width = this.Width - 200;
+
+        }
+    }
+}
