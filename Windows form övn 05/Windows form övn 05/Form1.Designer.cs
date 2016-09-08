4f73c80b3727c0564e8b06d1bec2d66218711b87
Tobbe <Tobbe@DESKTOP-2V46PR9>
2016-09-08 14:54:11 +0200

    övn 5

--- Windows form övn 05/Windows form övn 05/Form1.Designer.cs
+++ Windows form övn 05/Windows form övn 05/Form1.Designer.cs
@@ -28,30 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxSSN = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.comboBoxMaritalStatus = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonOk
             // 
-            this.button1.Location = new System.Drawing.Point(61, 367);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(44, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ok";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonOk.Location = new System.Drawing.Point(61, 367);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(44, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -98,52 +98,52 @@
             this.label5.TabIndex = 5;
             this.label5.Text = "Phone number";
             // 
-            // textBox1
+            // textBoxFirstName
             // 
-            this.textBox1.Location = new System.Drawing.Point(61, 56);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 6;
+            this.textBoxFirstName.Location = new System.Drawing.Point(61, 56);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFirstName.TabIndex = 6;
             // 
-            // textBox2
+            // textBoxLastName
             // 
-            this.textBox2.Location = new System.Drawing.Point(61, 102);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 7;
+            this.textBoxLastName.Location = new System.Drawing.Point(61, 102);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxLastName.TabIndex = 7;
             // 
-            // textBox3
+            // textBoxSSN
             // 
-            this.textBox3.Location = new System.Drawing.Point(61, 148);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 8;
+            this.textBoxSSN.Location = new System.Drawing.Point(61, 148);
+            this.textBoxSSN.Name = "textBoxSSN";
+            this.textBoxSSN.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSSN.TabIndex = 8;
             // 
-            // textBox4
+            // textBoxEmail
             // 
-            this.textBox4.Location = new System.Drawing.Point(61, 194);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 9;
+            this.textBoxEmail.Location = new System.Drawing.Point(61, 194);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 22);
+            this.textBoxEmail.TabIndex = 9;
             // 
-            // textBox5
+            // textBoxPhoneNumber
             // 
-            this.textBox5.Location = new System.Drawing.Point(61, 240);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
-            this.textBox5.TabIndex = 10;
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(61, 240);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPhoneNumber.TabIndex = 10;
             // 
-            // comboBox1
+            // comboBoxMaritalStatus
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxMaritalStatus.FormattingEnabled = true;
+            this.comboBoxMaritalStatus.Items.AddRange(new object[] {
             "Married",
             "Not Married",
             "Unknown"});
-            this.comboBox1.Location = new System.Drawing.Point(61, 304);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 11;
+            this.comboBoxMaritalStatus.Location = new System.Drawing.Point(61, 304);
+            this.comboBoxMaritalStatus.Name = "comboBoxMaritalStatus";
+            this.comboBoxMaritalStatus.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMaritalStatus.TabIndex = 11;
             // 
             // label6
             // 
@@ -160,18 +160,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(336, 421);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxMaritalStatus);
+            this.Controls.Add(this.textBoxPhoneNumber);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxSSN);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonOk);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -181,18 +181,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxSSN;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.ComboBox comboBoxMaritalStatus;
         private System.Windows.Forms.Label label6;
     }
 }
--- Windows form övn 05/Windows form övn 05/Form1.cs
+++ Windows form övn 05/Windows form övn 05/Form1.cs
@@ -19,6 +19,46 @@ namespace Windows_form_övn_05
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string first = textBoxFirstName.Text;
+            string last = textBoxLastName.Text;
+            string ssn = textBoxSSN.Text;
+            string email = textBoxEmail.Text;
+            string phone = textBoxPhoneNumber.Text;
+            string merital = comboBoxMaritalStatus.Text;
+
+            StringBuilder checkEmpty = new StringBuilder();
+
+            if (first != "" && last != "" && ssn != "" && email != "" && phone != "" && merital != "")
+            {
+                MessageBox.Show("OK!");                
+            }
+            if (first == "")
+            {
+                checkEmpty.Append("First ");
+            }
+            if (last == "")
+            {
+                checkEmpty.Append(" Last ");
+            }
+            if (ssn == "")
+            {
+                checkEmpty.Append(" SSN ");
+            }
+            if (email == "")
+            {
+                checkEmpty.Append(" Email ");
+            }
+            if (phone == "")
+            {
+                checkEmpty.Append(" Phone ");
+            }
+            if (merital == "")
+            {
+                checkEmpty.Append(" Merital");
+            }
+            if (checkEmpty.ToString() != "")
+                MessageBox.Show(("Följande är inte ifyllda: ") + checkEmpty.ToString());
+
 
         }
     }
