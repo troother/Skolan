32d0e8d7a3411c09302c98807daa481dda6f04c0
Tobbe <Tobbe@DESKTOP-2V46PR9>
2016-09-15 09:22:27 +0200

    .

---
+++ Objektorgentering övn 05/Objektorgentering övn 05/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektorgentering_övn_05
+{
+    class Student        
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int ssn { get; set; }
+        public string email { get; set; }
+
+        public Student(string val1, string val2, int val3, string val4)
+        {
+            firstName = val1;
+            lastName = val2;
+            ssn = val3;
+            email = val4;
+        }
+
+        public string controllvalues()
+        {
+            if ()
+            
+        }
+
+    }
+}
---
+++ Objektorientering övn 02/Objektorientering övn 02/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektorientering_övn_02
+{
+    class Course
+    {
+        public string CourseName { get; set; }
+        public int Points { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public double calculateNumberOfDays()
+        {
+            var amountOfDays = EndDate - StartDate;
+            TimeSpan totdays = EndDate.Subtract(StartDate);
+            double totday = totdays.TotalDays;
+            return totday;
+        }
+    }
+}
