a4cdbddd4b41ded1f0e536c09af5f486f9575263
Tobbe <Tobbe@DESKTOP-2V46PR9>
2016-09-16 10:26:24 +0200

    .

--- Inlämningsuppgift 01/Inlämningsuppgift 01/Form1.Designer.cs
+++ Inlämningsuppgift 01/Inlämningsuppgift 01/Form1.Designer.cs
@@ -28,91 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.groupBoxInformation = new System.Windows.Forms.GroupBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSaveNew = new System.Windows.Forms.Button();
+            this.btnSaveChange = new System.Windows.Forms.Button();
+            this.txtLastName = new System.Windows.Forms.TextBox();
             this.lblLastName = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.lblEmail = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
             this.lblPhoneNumber = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
             this.lblCity = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
             this.lblZipCode = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
             this.lblStreetAdress = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
             this.lblFirstName = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.listViewPersons = new System.Windows.Forms.ListView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBoxPeople = new System.Windows.Forms.GroupBox();
+            this.groupBoxInformation.SuspendLayout();
+            this.groupBoxPeople.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox2
+            // groupBoxInformation
             // 
-            this.groupBox2.Controls.Add(this.button4);
-            this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.textBox7);
-            this.groupBox2.Controls.Add(this.lblLastName);
-            this.groupBox2.Controls.Add(this.textBox6);
-            this.groupBox2.Controls.Add(this.lblEmail);
-            this.groupBox2.Controls.Add(this.textBox5);
-            this.groupBox2.Controls.Add(this.lblPhoneNumber);
-            this.groupBox2.Controls.Add(this.textBox4);
-            this.groupBox2.Controls.Add(this.lblCity);
-            this.groupBox2.Controls.Add(this.textBox3);
-            this.groupBox2.Controls.Add(this.lblZipCode);
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.lblStreetAdress);
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Controls.Add(this.lblFirstName);
-            this.groupBox2.Location = new System.Drawing.Point(219, 13);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(335, 294);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Information";
+            this.groupBoxInformation.Controls.Add(this.btnRemove);
+            this.groupBoxInformation.Controls.Add(this.btnSaveNew);
+            this.groupBoxInformation.Controls.Add(this.btnSaveChange);
+            this.groupBoxInformation.Controls.Add(this.txtLastName);
+            this.groupBoxInformation.Controls.Add(this.lblLastName);
+            this.groupBoxInformation.Controls.Add(this.txtEmail);
+            this.groupBoxInformation.Controls.Add(this.lblEmail);
+            this.groupBoxInformation.Controls.Add(this.txtPhone);
+            this.groupBoxInformation.Controls.Add(this.lblPhoneNumber);
+            this.groupBoxInformation.Controls.Add(this.txtCity);
+            this.groupBoxInformation.Controls.Add(this.lblCity);
+            this.groupBoxInformation.Controls.Add(this.txtZipCode);
+            this.groupBoxInformation.Controls.Add(this.lblZipCode);
+            this.groupBoxInformation.Controls.Add(this.txtStreetAddress);
+            this.groupBoxInformation.Controls.Add(this.lblStreetAdress);
+            this.groupBoxInformation.Controls.Add(this.txtFirstName);
+            this.groupBoxInformation.Controls.Add(this.lblFirstName);
+            this.groupBoxInformation.Location = new System.Drawing.Point(219, 13);
+            this.groupBoxInformation.Name = "groupBoxInformation";
+            this.groupBoxInformation.Size = new System.Drawing.Size(335, 294);
+            this.groupBoxInformation.TabIndex = 1;
+            this.groupBoxInformation.TabStop = false;
+            this.groupBoxInformation.Text = "Information";
             // 
-            // button4
+            // btnRemove
             // 
-            this.button4.Location = new System.Drawing.Point(223, 243);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(103, 45);
-            this.button4.TabIndex = 16;
-            this.button4.Text = "Remove";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnRemove.Location = new System.Drawing.Point(223, 243);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(103, 45);
+            this.btnRemove.TabIndex = 16;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnSaveNew
             // 
-            this.button3.Location = new System.Drawing.Point(6, 243);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(103, 45);
-            this.button3.TabIndex = 15;
-            this.button3.Text = "Save New";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSaveNew.Location = new System.Drawing.Point(6, 243);
+            this.btnSaveNew.Name = "btnSaveNew";
+            this.btnSaveNew.Size = new System.Drawing.Size(103, 45);
+            this.btnSaveNew.TabIndex = 15;
+            this.btnSaveNew.Text = "Save New";
+            this.btnSaveNew.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnSaveChange
             // 
-            this.button1.Location = new System.Drawing.Point(115, 243);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 45);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Save changes";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSaveChange.Location = new System.Drawing.Point(115, 243);
+            this.btnSaveChange.Name = "btnSaveChange";
+            this.btnSaveChange.Size = new System.Drawing.Size(103, 45);
+            this.btnSaveChange.TabIndex = 14;
+            this.btnSaveChange.Text = "Save changes";
+            this.btnSaveChange.UseVisualStyleBackColor = true;
             // 
-            // textBox7
+            // txtLastName
             // 
-            this.textBox7.Location = new System.Drawing.Point(116, 48);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(210, 22);
-            this.textBox7.TabIndex = 13;
+            this.txtLastName.Location = new System.Drawing.Point(116, 48);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(210, 22);
+            this.txtLastName.TabIndex = 13;
             // 
             // lblLastName
             // 
@@ -123,12 +123,12 @@
             this.lblLastName.TabIndex = 12;
             this.lblLastName.Text = "Last name:";
             // 
-            // textBox6
+            // txtEmail
             // 
-            this.textBox6.Location = new System.Drawing.Point(116, 203);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(210, 22);
-            this.textBox6.TabIndex = 11;
+            this.txtEmail.Location = new System.Drawing.Point(116, 203);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(210, 22);
+            this.txtEmail.TabIndex = 11;
             // 
             // lblEmail
             // 
@@ -139,12 +139,12 @@
             this.lblEmail.TabIndex = 10;
             this.lblEmail.Text = "Email:";
             // 
-            // textBox5
+            // txtPhone
             // 
-            this.textBox5.Location = new System.Drawing.Point(116, 172);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(210, 22);
-            this.textBox5.TabIndex = 9;
+            this.txtPhone.Location = new System.Drawing.Point(116, 172);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(210, 22);
+            this.txtPhone.TabIndex = 9;
             // 
             // lblPhoneNumber
             // 
@@ -155,12 +155,12 @@
             this.lblPhoneNumber.TabIndex = 8;
             this.lblPhoneNumber.Text = "Phone number:";
             // 
-            // textBox4
+            // txtCity
             // 
-            this.textBox4.Location = new System.Drawing.Point(116, 141);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(210, 22);
-            this.textBox4.TabIndex = 7;
+            this.txtCity.Location = new System.Drawing.Point(116, 141);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(210, 22);
+            this.txtCity.TabIndex = 7;
             // 
             // lblCity
             // 
@@ -171,12 +171,12 @@
             this.lblCity.TabIndex = 6;
             this.lblCity.Text = "City:";
             // 
-            // textBox3
+            // txtZipCode
             // 
-            this.textBox3.Location = new System.Drawing.Point(116, 110);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(210, 22);
-            this.textBox3.TabIndex = 5;
+            this.txtZipCode.Location = new System.Drawing.Point(116, 110);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(210, 22);
+            this.txtZipCode.TabIndex = 5;
             // 
             // lblZipCode
             // 
@@ -187,12 +187,12 @@
             this.lblZipCode.TabIndex = 4;
             this.lblZipCode.Text = "Zip code:";
             // 
-            // textBox2
+            // txtStreetAddress
             // 
-            this.textBox2.Location = new System.Drawing.Point(116, 79);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(210, 22);
-            this.textBox2.TabIndex = 3;
+            this.txtStreetAddress.Location = new System.Drawing.Point(116, 79);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(210, 22);
+            this.txtStreetAddress.TabIndex = 3;
             // 
             // lblStreetAdress
             // 
@@ -203,12 +203,12 @@
             this.lblStreetAdress.TabIndex = 2;
             this.lblStreetAdress.Text = "Street adress:";
             // 
-            // textBox1
+            // txtFirstName
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 18);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(210, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtFirstName.Location = new System.Drawing.Point(116, 18);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(210, 22);
+            this.txtFirstName.TabIndex = 1;
             // 
             // lblFirstName
             // 
@@ -219,83 +219,83 @@
             this.lblFirstName.TabIndex = 0;
             this.lblFirstName.Text = "First name:";
             // 
-            // listView1
+            // listViewPersons
             // 
-            this.listView1.Location = new System.Drawing.Point(6, 53);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(187, 233);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listViewPersons.Location = new System.Drawing.Point(6, 53);
+            this.listViewPersons.Name = "listViewPersons";
+            this.listViewPersons.Size = new System.Drawing.Size(187, 233);
+            this.listViewPersons.TabIndex = 0;
+            this.listViewPersons.UseCompatibleStateImageBehavior = false;
             // 
-            // textBox8
+            // txtSearch
             // 
-            this.textBox8.Location = new System.Drawing.Point(6, 22);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(116, 22);
-            this.textBox8.TabIndex = 1;
+            this.txtSearch.Location = new System.Drawing.Point(6, 22);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(116, 22);
+            this.txtSearch.TabIndex = 1;
             // 
-            // button2
+            // btnSearch
             // 
-            this.button2.Location = new System.Drawing.Point(128, 22);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(65, 25);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(128, 22);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(65, 25);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // groupBoxPeople
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.textBox8);
-            this.groupBox1.Controls.Add(this.listView1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 13);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 294);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Peoples";
+            this.groupBoxPeople.Controls.Add(this.btnSearch);
+            this.groupBoxPeople.Controls.Add(this.txtSearch);
+            this.groupBoxPeople.Controls.Add(this.listViewPersons);
+            this.groupBoxPeople.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxPeople.Name = "groupBoxPeople";
+            this.groupBoxPeople.Size = new System.Drawing.Size(200, 294);
+            this.groupBoxPeople.TabIndex = 0;
+            this.groupBoxPeople.TabStop = false;
+            this.groupBoxPeople.Text = "Peoples";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 314);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxInformation);
+            this.Controls.Add(this.groupBoxPeople);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.groupBoxInformation.ResumeLayout(false);
+            this.groupBoxInformation.PerformLayout();
+            this.groupBoxPeople.ResumeLayout(false);
+            this.groupBoxPeople.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.GroupBox groupBoxInformation;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.Label lblLastName;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtPhone;
         private System.Windows.Forms.Label lblPhoneNumber;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtCity;
         private System.Windows.Forms.Label lblCity;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtZipCode;
         private System.Windows.Forms.Label lblZipCode;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtStreetAddress;
         private System.Windows.Forms.Label lblStreetAdress;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSaveNew;
+        private System.Windows.Forms.Button btnSaveChange;
         private System.Windows.Forms.Label lblFirstName;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listViewPersons;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBoxPeople;
     }
 }
 
