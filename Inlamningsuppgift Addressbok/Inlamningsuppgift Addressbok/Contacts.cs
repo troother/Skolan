14f240df20e5920df646f3b5439550bf03f79fbc
Troother <Troother@DESKTOP-BRBQ07J>
2016-09-16 15:30:05 +0200

    Update

--- Inlamningsuppgift Addressbok/Inlamningsuppgift Addressbok/Contacts.cs
+++ Inlamningsuppgift Addressbok/Inlamningsuppgift Addressbok/Contacts.cs
@@ -6,18 +6,13 @@ using System.Threading.Tasks;
 
 namespace Inlamningsuppgift_Addressbok
 {
-    class Contact
+    public class Contact
     {       
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
-
         public string Address { get; set; }
-
         public string ZipCode { get; set; }
-
         public string Phone { get; set; }
-
         public string Email { get; set; }
     }
 }
