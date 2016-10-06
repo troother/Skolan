c23f800411af78116047db7e360686494e17bafc
Troother <Troother@DESKTOP-BRBQ07J>
2016-10-06 15:11:55 +0200

    Update

--- Inlamningsuppgift Shotgun/Inlamningsuppgift Shotgun/Form1.Designer.cs
+++ Inlamningsuppgift Shotgun/Inlamningsuppgift Shotgun/Form1.Designer.cs
@@ -28,12 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPlayer1Shot = new System.Windows.Forms.Button();
+            this.btnPlayer1Load = new System.Windows.Forms.Button();
+            this.btnPlayer1Block = new System.Windows.Forms.Button();
+            this.btnPlayer1Shotgun = new System.Windows.Forms.Button();
+            this.picBoxPlayer1shell1 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer1shell2 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer1shell3 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer2shell3 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer2shell2 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer2shell1 = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer2Pic = new System.Windows.Forms.PictureBox();
+            this.picBoxPlayer1Pic = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1shell1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1shell2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1shell3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2shell3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2shell2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2shell1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlayer1Shot
+            // 
+            this.btnPlayer1Shot.Location = new System.Drawing.Point(217, 463);
+            this.btnPlayer1Shot.Name = "btnPlayer1Shot";
+            this.btnPlayer1Shot.Size = new System.Drawing.Size(89, 38);
+            this.btnPlayer1Shot.TabIndex = 0;
+            this.btnPlayer1Shot.Text = "Shot";
+            this.btnPlayer1Shot.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayer1Load
+            // 
+            this.btnPlayer1Load.Location = new System.Drawing.Point(312, 463);
+            this.btnPlayer1Load.Name = "btnPlayer1Load";
+            this.btnPlayer1Load.Size = new System.Drawing.Size(89, 38);
+            this.btnPlayer1Load.TabIndex = 1;
+            this.btnPlayer1Load.Text = "Load";
+            this.btnPlayer1Load.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayer1Block
+            // 
+            this.btnPlayer1Block.Location = new System.Drawing.Point(407, 463);
+            this.btnPlayer1Block.Name = "btnPlayer1Block";
+            this.btnPlayer1Block.Size = new System.Drawing.Size(89, 39);
+            this.btnPlayer1Block.TabIndex = 2;
+            this.btnPlayer1Block.Text = "Block";
+            this.btnPlayer1Block.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayer1Shotgun
+            // 
+            this.btnPlayer1Shotgun.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayer1Shotgun.Location = new System.Drawing.Point(217, 415);
+            this.btnPlayer1Shotgun.Name = "btnPlayer1Shotgun";
+            this.btnPlayer1Shotgun.Size = new System.Drawing.Size(279, 42);
+            this.btnPlayer1Shotgun.TabIndex = 3;
+            this.btnPlayer1Shotgun.Text = "   Shotgun   ";
+            this.btnPlayer1Shotgun.UseVisualStyleBackColor = true;
+            // 
+            // picBoxPlayer1shell1
+            // 
+            this.picBoxPlayer1shell1.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer1shell1.Image")));
+            this.picBoxPlayer1shell1.Location = new System.Drawing.Point(85, 332);
+            this.picBoxPlayer1shell1.Name = "picBoxPlayer1shell1";
+            this.picBoxPlayer1shell1.Size = new System.Drawing.Size(46, 50);
+            this.picBoxPlayer1shell1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer1shell1.TabIndex = 8;
+            this.picBoxPlayer1shell1.TabStop = false;
+            // 
+            // picBoxPlayer1shell2
+            // 
+            this.picBoxPlayer1shell2.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer1shell2.Image")));
+            this.picBoxPlayer1shell2.Location = new System.Drawing.Point(137, 332);
+            this.picBoxPlayer1shell2.Name = "picBoxPlayer1shell2";
+            this.picBoxPlayer1shell2.Size = new System.Drawing.Size(46, 50);
+            this.picBoxPlayer1shell2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer1shell2.TabIndex = 9;
+            this.picBoxPlayer1shell2.TabStop = false;
+            // 
+            // picBoxPlayer1shell3
+            // 
+            this.picBoxPlayer1shell3.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer1shell3.Image")));
+            this.picBoxPlayer1shell3.Location = new System.Drawing.Point(189, 332);
+            this.picBoxPlayer1shell3.Name = "picBoxPlayer1shell3";
+            this.picBoxPlayer1shell3.Size = new System.Drawing.Size(46, 50);
+            this.picBoxPlayer1shell3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer1shell3.TabIndex = 10;
+            this.picBoxPlayer1shell3.TabStop = false;
+            // 
+            // picBoxPlayer2shell3
+            // 
+            this.picBoxPlayer2shell3.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer2shell3.Image")));
+            this.picBoxPlayer2shell3.Location = new System.Drawing.Point(584, 332);
+            this.picBoxPlayer2shell3.Name = "picBoxPlayer2shell3";
+            this.picBoxPlayer2shell3.Size = new System.Drawing.Size(46, 50);
+            this.picBoxPlayer2shell3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer2shell3.TabIndex = 13;
+            this.picBoxPlayer2shell3.TabStop = false;
+            // 
+            // picBoxPlayer2shell2
+            // 
+            this.picBoxPlayer2shell2.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer2shell2.Image")));
+            this.picBoxPlayer2shell2.Location = new System.Drawing.Point(532, 332);
+            this.picBoxPlayer2shell2.Name = "picBoxPlayer2shell2";
+            this.picBoxPlayer2shell2.Size = new System.Drawing.Size(46, 50);
+            this.picBoxPlayer2shell2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer2shell2.TabIndex = 12;
+            this.picBoxPlayer2shell2.TabStop = false;
+            // 
+            // picBoxPlayer2shell1
+            // 
+            this.picBoxPlayer2shell1.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer2shell1.Image")));
+            this.picBoxPlayer2shell1.Location = new System.Drawing.Point(480, 332);
+            this.picBoxPlayer2shell1.Name = "picBoxPlayer2shell1";
+            this.picBoxPlayer2shell1.Size = new System.Drawing.Size(46, 50);
+            this.picBoxPlayer2shell1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer2shell1.TabIndex = 11;
+            this.picBoxPlayer2shell1.TabStop = false;
+            // 
+            // picBoxPlayer2Pic
+            // 
+            this.picBoxPlayer2Pic.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer2Pic.Image")));
+            this.picBoxPlayer2Pic.Location = new System.Drawing.Point(405, 123);
+            this.picBoxPlayer2Pic.Name = "picBoxPlayer2Pic";
+            this.picBoxPlayer2Pic.Size = new System.Drawing.Size(295, 203);
+            this.picBoxPlayer2Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer2Pic.TabIndex = 14;
+            this.picBoxPlayer2Pic.TabStop = false;
+            // 
+            // picBoxPlayer1Pic
+            // 
+            this.picBoxPlayer1Pic.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPlayer1Pic.Image")));
+            this.picBoxPlayer1Pic.Location = new System.Drawing.Point(12, 123);
+            this.picBoxPlayer1Pic.Name = "picBoxPlayer1Pic";
+            this.picBoxPlayer1Pic.Size = new System.Drawing.Size(279, 203);
+            this.picBoxPlayer1Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxPlayer1Pic.TabIndex = 15;
+            this.picBoxPlayer1Pic.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(231, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(721, 548);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picBoxPlayer1Pic);
+            this.Controls.Add(this.picBoxPlayer2Pic);
+            this.Controls.Add(this.picBoxPlayer2shell3);
+            this.Controls.Add(this.picBoxPlayer2shell2);
+            this.Controls.Add(this.picBoxPlayer2shell1);
+            this.Controls.Add(this.picBoxPlayer1shell3);
+            this.Controls.Add(this.picBoxPlayer1shell2);
+            this.Controls.Add(this.picBoxPlayer1shell1);
+            this.Controls.Add(this.btnPlayer1Shotgun);
+            this.Controls.Add(this.btnPlayer1Block);
+            this.Controls.Add(this.btnPlayer1Load);
+            this.Controls.Add(this.btnPlayer1Shot);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1shell1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1shell2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1shell3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2shell3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2shell2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2shell1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer2Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlayer1Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPlayer1Shot;
+        private System.Windows.Forms.Button btnPlayer1Load;
+        private System.Windows.Forms.Button btnPlayer1Block;
+        private System.Windows.Forms.Button btnPlayer1Shotgun;
+        private System.Windows.Forms.PictureBox picBoxPlayer1shell1;
+        private System.Windows.Forms.PictureBox picBoxPlayer1shell2;
+        private System.Windows.Forms.PictureBox picBoxPlayer1shell3;
+        private System.Windows.Forms.PictureBox picBoxPlayer2shell3;
+        private System.Windows.Forms.PictureBox picBoxPlayer2shell2;
+        private System.Windows.Forms.PictureBox picBoxPlayer2shell1;
+        private System.Windows.Forms.PictureBox picBoxPlayer2Pic;
+        private System.Windows.Forms.PictureBox picBoxPlayer1Pic;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
